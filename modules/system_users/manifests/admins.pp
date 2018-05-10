e0f458e394d7c2abc83a686f1aa43955504ebc33
nmk333 <nmkhan333@gmail.com>
2018-05-10 14:52:20 +0000

    fixed groups

--- modules/system_users/manifests/admins.pp
+++ modules/system_users/manifests/admins.pp
@@ -1,15 +1,22 @@
 class system_users::admins {
-package {'csh':
-ensure => latest
+group {staff':
+ensure=>present
 }
 
-group { 'staff':
-ensure => present,
-}
+if $facts['kernal'] == 'windows' {
 user {'admin':
-group => 'start',
+group=> 'staff'
+}
+else
+{
+package {'csh':
+ensure => latest,
+}
+
+user {'admin':
+groups => 'start',
 shell => '/bin/csh',
 require => Package ['csh'],
 }
 }
-
+}
